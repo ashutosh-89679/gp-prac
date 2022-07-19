682a36616e5f6a5cf91faf2a7753d8ace27e4e56
DIVIПΣ89 <71063368+ashutosh-89679@users.noreply.github.com>
2022-07-19 09:58:37 +0530

    prac1 code

---
+++ code.cpp
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.DirectX;
+using Microsoft.DirectX.Direct3D;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        private Device device;
+        public Form1()
+        {
+            InitializeComponent();
+            InitDevice();
+        }
+
+        private void InitDevice()
+        {   
+            
+           PresentParameters pp = new PresentParameters();
+           pp.Windowed= true;
+           pp.SwapEffect= SwapEffect.Discard;
+           device = new Device(0, DeviceType.Hardware, this, CreateFlags.HardwareVertexProcessing, pp);
+
+        }
+
+       
+        private void Render()
+        {
+            device.Clear(ClearFlags.Target, Color.Orange, 0, 1);
+            device.Present();
+
+            
+
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Render();
+        }
+
+    }
+}
